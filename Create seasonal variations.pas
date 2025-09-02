01252c704243ea3fea0d9d6a29e9d87e47bb2842
matm-git <78607219+matm-git@users.noreply.github.com>
2025-09-03 00:46:27 +0200

    Add files via upload

--- Create seasonal variations.pas
+++ Create seasonal variations.pas
@@ -1,5 +1,7 @@
 unit UserScript;
-
+//This script duplicates FormIds from an input file and creates 4 seasonal copies (SPR,SUM,AUT,WIN) in a targetFileName
+//It requires to set inputFileName (default 'Input records for Seasons conversion.esp') in the script itself and that plugin must be loaded in xEdit. All FormIds of this plugin will be read.
+//It requires to set targetFileName (default ''PBR Flora Overhaul Seasons.esp') in the script itself and that plugin must be loaded in xEdit. This will be used to write the output FormIds.
 var
   targetFile, inputFile: IInterface;
   outFile: TextFile;
@@ -9,24 +11,27 @@ var
   i, j, recCount: Integer;
   srcRec, newRec, edidElem, targetRecord: IInterface;
   edid, suffix, newEdid: string;
+  inputFileName, targetFileName: string;
   suffixes: array[0..3] of string;
   modelElem: IInterface;
   oldModelPath, newModelPath: string;  
 begin
-  Result := 0;
-
   // Zielplugin festlegen
-  targetFile := FileByName('PBR Flora Overhaul Seasons.esp');
+  inputFileName :='Input records for Seasons conversion.esp';
+  targetFileName := 'PBR Flora Overhaul Seasons.esp';
+  
+  Result := 0;
+  targetFile := FileByName(targetFileName);
   if not Assigned(targetFile) then begin
-    AddMessage('Zieldatei "PBR Flora Overhaul Seasons.esp" nicht geladen!');
+    AddMessage('Zieldatei "' + targetFileName + '" nicht geladen!');
     Result := 1;
     Exit;
   end;
 
   // Inputplugin festlegen 
-  inputFile := FileByName('Records for Seasons conversion.esp');
+  inputFile := FileByName(inputFileName);
   if not Assigned(inputFile) then begin
-    AddMessage('Eingabedatei "Records for Seasons conversion.esp" nicht geladen!');
+    AddMessage('Eingabedatei "Records for ' + inputFileName +'" nicht geladen!');
     Result := 1;
     Exit;
   end;
@@ -38,7 +43,7 @@ begin
   suffixes[2] := 'AUT';
   suffixes[3] := 'WIN';
 
-  // Alle Records aus Input.esp durchgehen
+  // Alle Records aus Input durchgehen
   for i := 0 to Pred(RecordCount(inputFile)) do begin
     srcRec := RecordByIndex(inputFile, i);
     if not Assigned(srcRec) then Continue;
@@ -88,7 +93,7 @@ begin
    
       AddMessage('0x' + Copy(IntToHex(FixedFormID(srcRec), 8),4,5) + '~Skyrim.esm|' +
                        '0x' + Copy(IntToHex(FixedFormID(newRec), 8),4,5) + 
-                       '~PBR Flora Overhaul Seasons.esp');  
+                       '~'+targetFileName);  
  
     end;
   end;
